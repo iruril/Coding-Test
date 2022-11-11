2daca7515637199c875dc55742c4a848f6afe6d1
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-11 21:38:08 +0900

    221111 코딩테스트연습(3)
    <프로그래머스>
    연습문제 - 피보나치 수

---
+++ Programmers-Fibonacci/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Programmers_Fibonacci
+{
+    class Program
+    {
+        public static int Fibo(int n)
+        {
+            int firstIdx = 1;
+            int secondIdx = 1;
+            int resultIdx = 0;
+
+            if (n <= 2) return 1;
+            for (int i = 3; i <= n; i++)
+            {
+                resultIdx = (firstIdx + secondIdx) % 1234567;
+                firstIdx = secondIdx;
+                secondIdx = resultIdx;
+            }
+            return resultIdx;
+        }
+
+        public int solution(int n)
+        {
+            int answer = 0;
+            int fibo_n = Fibo(n);
+
+            answer = fibo_n;
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", Fibo(3));
+            Console.WriteLine("{0}", Fibo(5));
+        }
+    }
+}
