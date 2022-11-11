d7abee357899ea35dbe34ee3150a99a3b4630bfb
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-11 19:14:13 +0900

    221111 코딩테스트 연습
    <프로그래머스>
    kakao 숫자 문자열과 영단어

---
+++ Programmers-Words_and_Numbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Programmers_Words_and_Numbers
+{
+    class Program
+    {
+        public static int solution(string s)
+        {
+            int answer = 0;
+            string answerStr = s;
+
+            answerStr = answerStr.Replace("zero", "0");
+            answerStr = answerStr.Replace("one", "1");
+            answerStr = answerStr.Replace("two", "2");
+            answerStr = answerStr.Replace("three", "3");
+            answerStr = answerStr.Replace("four", "4");
+            answerStr = answerStr.Replace("five", "5");
+            answerStr = answerStr.Replace("six", "6");
+            answerStr = answerStr.Replace("seven", "7");
+            answerStr = answerStr.Replace("eight", "8");
+            answerStr = answerStr.Replace("nine", "9");
+
+            answer = int.Parse(answerStr);
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            string s = "2three45sixseven";
+            Console.WriteLine("{0}", solution(s));
+        }
+    }
+}
