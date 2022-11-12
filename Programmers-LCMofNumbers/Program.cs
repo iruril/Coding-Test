63e79fc56ce027e2cd91a4e32954cf7d697eb014
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-12 22:11:50 +0900

    221112 코딩테스트연습
    <Programmers>
    연습문제 - N개의 최소공배수

---
+++ Programmers-LCMofNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Programmers_LCMofNumbers
+{
+    class Program
+    {
+        public static int solution(int[] arr)
+        {
+            int answer = 0;
+            int len = arr.Length;
+            int max = arr[len - 1];
+            int cnt = 0;
+
+            for (int i = 1; ; i++)
+            {
+                for (int j = 0; j < len - 1; j++)
+                {
+                    if ((max * i) % arr[j] == 0)
+                    {
+                        cnt++;
+                    }
+                }
+                if (cnt == len - 1)
+                {
+                    answer = max * i;
+                    return answer;
+                }
+                cnt = 0;
+            }
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 6, 8, 14 };
+            Console.WriteLine("{0}",solution(a));
+        }
+    }
+}
