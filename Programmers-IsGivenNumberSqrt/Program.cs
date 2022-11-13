6098c005a60eff8fe7e6e733ee49cfdbc4799d43
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-13 14:53:26 +0900

    221113 코딩테스트연습
    <Programmers>
    연습문제 - 정수 제곱근 판별

---
+++ Programmers-IsGivenNumberSqrt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Programmers_IsGivenNumberSqrt
+{
+    class Program
+    {
+        static public long solution(long n)
+        {
+            long answer = 0;
+            var sqr = Math.Sqrt(n);
+            answer = (long)sqr;
+            if (sqr % 1 != 0)
+            {
+                return -1;
+            }
+            answer = (long)Math.Pow(answer + 1, 2);
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}",solution(121));
+            Console.WriteLine("{0}",solution(3));
+        }
+    }
+}
