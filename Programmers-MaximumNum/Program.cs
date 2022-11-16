357cdb62061dc6434e62081ddfc41036b4adab25
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-16 16:53:29 +0900

    221116 코딩테스트연습
    <Programmers>
    연습문제 - 가장 큰 수
    
    *람다식의 활용과, String.Compare의 이해, 그리고 String.Join의 활용에 집중하자.
    
    *String을 +=하는 데에 생각보다 많은 시간이 소요된다. 그러니, 가급적이면 정수 배열을 옮길 때는 String.Join이 좋을 것 같다.

---
+++ Programmers-MaximumNum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Programmers_MaximumNum
+{
+    class Program
+    {
+        public static string solution(int[] numbers)
+        {
+            string answer = "";
+            Array.Sort(numbers, (x, y) => string.Compare(y.ToString() + x.ToString(), x.ToString() + y.ToString()));
+
+            answer = String.Join("", numbers);
+
+            int cnt = 0;
+            foreach (var i in answer)
+            {
+                if (i == '0') cnt++;
+            }
+
+            if (cnt == answer.Length) return "0";
+            else return answer;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 6, 10, 2 };
+            int[] b = { 3, 30, 34, 5, 9 };
+            Console.WriteLine(solution(a));
+            Console.WriteLine(solution(b));
+        }
+    }
+}
