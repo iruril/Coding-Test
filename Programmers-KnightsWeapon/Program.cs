9cc5cf72ab5627a22d37badfc65418ae2df6586c
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-21 23:05:33 +0900

    221121 코딩테스트연습
    <Programmers>
    연습문제 - 기사단원의 무기
    
    *약수를 구하는 방법을 O(n^1/3)으로 줄이기가 가장 큰 관건.

---
+++ Programmers-KnightsWeapon/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Programmers_KnightsWeapon
+{
+    class Program
+    {
+        public static int weights(int n, int limit)
+        {
+            int cnt = 0;
+            int i;
+            for (i = 1; i * i < n; i++)
+            {
+                if (n % i == 0) cnt += 2;
+            }
+            if (i * i == n) cnt++;
+
+            if (cnt > limit) return 0;
+            else return cnt;
+        }
+
+        public static int solution(int number, int limit, int power)
+        {
+            int answer = 0;
+
+            for (int i = 1; i <= number; i++)
+            {
+                int temp = weights(i, limit);
+                if (temp != 0) answer += temp;
+                else answer += power;
+            }
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("{0}", solution(5, 3, 2));
+            Console.WriteLine("{0}", solution(10, 3, 2));
+        }
+    }
+}
