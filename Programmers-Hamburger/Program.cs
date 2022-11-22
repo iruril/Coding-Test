0d64e7287e1e547f4fe8e89329990800a2fac48e
Tae Wook Han <31647650+iruril@users.noreply.github.com>
2022-11-22 19:51:34 +0900

    221122 코딩테스트연습
    <Programmers>
    연습문제 - 햄버거 만들기
    
    *딱히 이렇다 할 어려움 포인트는 없었다. 단지, 리스트끼리 비교를 하고 싶으면 Linq를 추가해서 SequenceEqual을 사용하도록 하자, 정도.
    
    *항상 learn.microsoft.com 에서 다양한 기능들을 접하려고 노력하며 문제를 풀자. 그럴수록 더 코드는 더 간결해지고, 삶은 윤택해진다.

---
+++ Programmers-Hamburger/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Programmers_Hamburger
+{
+    class Program
+    {
+        public static int solution(int[] ingredient)
+        {
+            int answer = 0;
+            List<int> ingredientStack = new List<int>();
+            List<int> hamburger = new List<int>();
+            hamburger.Add(1);
+            hamburger.Add(2);
+            hamburger.Add(3);
+            hamburger.Add(1); //햄버거 완제품 예시
+
+            for (int i = 0; i < ingredient.Length; i++)
+            {
+                ingredientStack.Add(ingredient[i]);
+                if (ingredientStack.Count >= 4)
+                {
+                    List<int> temp = new List<int>();
+                    temp.Add(ingredientStack[ingredientStack.Count - 4]);
+                    temp.Add(ingredientStack[ingredientStack.Count - 3]);
+                    temp.Add(ingredientStack[ingredientStack.Count - 2]);
+                    temp.Add(ingredientStack[ingredientStack.Count - 1]);
+                    if (temp.SequenceEqual(hamburger))
+                    {
+                        ingredientStack.RemoveAt(ingredientStack.Count - 1);
+                        ingredientStack.RemoveAt(ingredientStack.Count - 1);
+                        ingredientStack.RemoveAt(ingredientStack.Count - 1);
+                        ingredientStack.RemoveAt(ingredientStack.Count - 1);
+                        answer++;
+                    }
+                }
+            }
+
+            return answer;
+        }
+        static void Main(string[] args)
+        {
+            int[] a = { 2, 1, 1, 2, 3, 1, 2, 3, 1 };
+            int[] b = { 1, 3, 2, 1, 2, 1, 3, 1, 2 };
+            Console.WriteLine("{0}", solution(a));
+            Console.WriteLine("{0}", solution(b));
+        }
+    }
+}
